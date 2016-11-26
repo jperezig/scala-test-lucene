7e0ba49b84e836d98682d3de61d766c265313a96
Joaquin Perez <joaquin.perez.iglesias@gmail.com>
2016-11-26 09:37:04 +0100

    Refactor class and test

--- src/main/scala/org/ninit/autosuggest/Autosuggest.scala
+++ src/main/scala/org/ninit/autosuggest/Autosuggest.scala
@@ -1,36 +1,40 @@
 package org.ninit.autosuggest
 
-import java.io.FileInputStream
+import java.io.{File, FileInputStream}
 
-import org.apache.lucene.analysis.es.SpanishAnalyzer
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.analysis.util.CharArraySet
 import org.apache.lucene.search.suggest.FileDictionary
-import org.apache.lucene.search.suggest.analyzing.{AnalyzingSuggester, FuzzySuggester}
+import org.apache.lucene.search.suggest.analyzing.FuzzySuggester
 import resource._
 
 import scala.collection.JavaConverters._
 
-object Autosuggest extends App {
+case class Suggestion(text: String, weight: Float)
 
-  def suggest(input: String, numResults: Int) = {
+class Autosuggest(file: File) {
 
-    val QueryAnalyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET)
-    val IndexAnalyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET)
+  val QueryAnalyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET)
+  val IndexAnalyzer = new StandardAnalyzer(CharArraySet.EMPTY_SET)
 
-    val suggester = new FuzzySuggester(IndexAnalyzer, QueryAnalyzer)
-
-    val InputFile = "/home/jperezi/Ep-20161020-employments-position-1M-jt_min10_score.csv"
-
-    for {
-      inputStream <- managed(new FileInputStream(InputFile))
-    } {
-      suggester.build(new FileDictionary(inputStream))
-    }
-
-    suggester.lookup(input, false, numResults).asScala
+  val suggester = new FuzzySuggester(IndexAnalyzer, QueryAnalyzer)
+  for {
+    inputStream <- managed(new FileInputStream(file))
+  } {
+    suggester.build(new FileDictionary(inputStream))
   }
 
-  suggest(args.mkString(" "), 10).foreach(r => println(s"'${r.key}'"))
+  def suggest(input: String, numResults: Int): Vector[Suggestion] = {
+    suggester.lookup(input, false, numResults).asScala.map { result =>
+      Suggestion(result.key.toString, result.value)
+    }.toVector
+  }
+}
+
+object Autosuggest extends App {
+
+  val suggester = new Autosuggest(new File(args.head))
+
+  suggester.suggest(args.tail.mkString(" "), 10).foreach(r => println(s"'${r.text}' (${r.weight})"))
 
 }
--- src/test/scala/org/ninit/autosuggest/AutosuggestTest.scala
+++ src/test/scala/org/ninit/autosuggest/AutosuggestTest.scala
@@ -1,17 +1,38 @@
 package org.ninit.autosuggest
 
-import org.scalatest.{FlatSpec, Matchers}
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
 
-/**
-  * Created by jperezi on 25/11/16.
-  */
-class AutosuggestTest extends FlatSpec with Matchers {
+import org.scalatest.{Matchers, Outcome, fixture}
 
-  "Autosuggest" should "autosuggest given an input" in {
-    Autosuggest.suggest("camarero", 10).head.key shouldBe "camarero"
+class AutosuggestTest extends fixture.FlatSpec with Matchers {
+
+  type FixtureParam = Autosuggest
+
+  def withFixture(test: OneArgTest): Outcome = {
+    val stringContent = "camarero\t10\ncamarera\t100\n"
+
+    val path = Files.createTempFile("titles", "tmp")
+    try {
+      Files.write(path, stringContent.getBytes(StandardCharsets.UTF_8))
+      val suggester = new Autosuggest(path.toFile)
+      test(suggester)
+    }
+    finally {
+      Files.delete(path)
+    }
   }
 
-  it should "return no more than max number of results" in {
-    Autosuggest.suggest("camarero", 1).length shouldBe 1
+  "Autosuggest" should "autosuggest perfect match at top" in { suggester =>
+    suggester.suggest("camarero", 10).head.text shouldBe "camarero"
+  }
+
+  it should "return result sorted by weight" in { suggester =>
+    suggester.suggest("camarer", 10).head shouldBe Suggestion("camarera", 100)
+  }
+
+  it should "return no more than max number of results" in { suggester =>
+    suggester.suggest("camarero", 1).length shouldBe 1
   }
 }
+
